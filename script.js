2ba84a04a7cd09fd0026297fb08561f391e7a142
FatemehQasemian <ghasemian29fatemeh@gmail.com>
2023-11-05 11:03:56 +0330

    clear button is fixed

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputItem = document.getElementById("item-input");
 const invalidInput = document.getElementById("input-invalid");
 const listItems = document.getElementById("item-list");
 const clearAllBtn = document.getElementById("items-clear");
+const itemFilter = document.getElementById("filter");
 // const btn = document.getElementById("btn");
 
 function addItem(e) {
@@ -21,12 +22,14 @@ function addItem(e) {
   li.appendChild(icon);
   listItems.appendChild(li);
   inputItem.value = "";
+  checkUI();
 }
 
 function onClickItem(e) {
   if (e.target.classList.contains("bi-x")) {
     e.target.parentElement.remove();
   }
+  checkUI();
 }
 
 function createIcon(classes) {
@@ -37,9 +40,27 @@ function createIcon(classes) {
 
 function clearAll(e) {
   listItems.innerText = "";
+  checkUI();
+}
+
+function checkUI() {
+  const len = listItems.querySelectorAll("li").length;
+  if (len == 0) {
+    clearAllBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearAllBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+function filterItems(e) {
+  console.log(e.target.value.toLowerCase());
 }
 
 // Event Listener
+checkUI();
 formItems.addEventListener("submit", addItem);
 listItems.addEventListener("click", onClickItem);
 clearAllBtn.addEventListener("click", clearAll);
+itemFilter.addEventListener("input", filterItems);
